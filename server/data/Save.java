c5b6f506ad595766d2e610b3113bca41344b5b4c
Nanson Chen <buyingsaucer2@gmail.com>
2025-09-20 16:21:02 -0400

    Made saving data possible

---
+++ server/data/Save.java
@@ -0,0 +1,21 @@
+package server.data;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Save {
+    public static void main(String[] args) {
+        File file = new File(new File("").getAbsolutePath() + "/server/data/data.txt");
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            if (args.length != 0)
+                fileWriter.write(args[0] + "\n");
+            else
+                fileWriter.write("Nope\n");
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
---
+++ server/data/save.js
@@ -0,0 +1,47 @@
+// All args after the script name
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.error("Usage: node save.js <arg> [more args]");
+  process.exit(1);
+}
+save(args);
+function save(args) {
+  const { spawn } = require("child_process");
+  const path = require("path");
+  const fs = require("fs");
+
+  const scriptDir = __dirname; // /.../server/data
+  const repoRoot = path.resolve(scriptDir, "..", "..");
+
+  // Look for compiled Save.class in common locations
+  const packageClassPath = path.join(repoRoot, "server", "data", "Save.class");
+  const rootClassPath = path.join(scriptDir, "Save.class");
+
+  let classpath;
+  let className;
+
+  if (fs.existsSync(packageClassPath)) {
+    classpath = repoRoot; // so `server.data.Save` is resolvable
+    className = "server.data.Save";
+  } else if (fs.existsSync(rootClassPath)) {
+    classpath = scriptDir; // so `Save` is resolvable
+    className = "Save";
+  } else {
+    // Default to package style (common in this project layout)
+    classpath = repoRoot;
+    className = "server.data.Save";
+    console.warn(
+      "Warning: Save.class not found in expected locations. Defaulting to",
+      className
+    );
+  }
+
+  const javaArgs = ["-cp", classpath, className, ...args];
+
+  // Forward stdio (stdout/stderr/stdin) to parent process for simplicity
+  const javaProcess = spawn("java", javaArgs, { stdio: "inherit" });
+
+  javaProcess.on("close", (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+}
